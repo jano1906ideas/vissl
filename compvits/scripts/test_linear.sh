bd010ba116c860fd2b76456d5244425d0bf7085f
Jan Olszewski <jan.olszewski@ideas-ncbr.pl>
2023-08-17 11:09:17 +0000

    added division masks, imported supervised vit, compvits/scripts/test_linear.sh reproduces deit, TODO: cleanup config files and test_linear.sh script

--- compvits/scripts/test_linear.sh
+++ compvits/scripts/test_linear.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 
-#cm=cm_98_98_196_0
-#model=ibot
-#K=0
+cm=cm_98_98_196_0
+model=supervised
+K=0
 
-cm=$1
-model=$2
-K=$3
-
+#cm=$1
+#model=$2
+#K=$3
+dir=$4
 echo test_linear: $cm $model $K
 
-dir="logs/test_linear/${cm}/${model}/K$K"
+if [[ $dir == "" ]]; then
+    dir="debug/test_linear/${cm}/${model}/K$K"
+fi
+
+
 python tools/run_distributed_engines.py \
-    config=compvits/vits_trunk \
-    +config/compvits/data/test=in1k_tiny \
-    +config/compvits/data/test/transforms=$cm \
-    +config/compvits/benchmark=test_linear \
+    config=compvits/deitb_trunk_head \
+    +config/compvits/data/test=in1k \
+    +config/compvits/task=test_linear \
     config.TEST_ONLY=True \
     config.CHECKPOINT.DIR=$dir \
-    config.MODEL.WEIGHTS_INIT.PARAMS_FILE=/home/jan.olszewski/git/vissl/checkpoints/ibot/vits_teacher_linear.pth \
-    config.MODEL.TRUNK.VISION_TRANSFORMERS.COMPVITS.NAME=afterK \
-    config.MODEL.TRUNK.VISION_TRANSFORMERS.COMPVITS.PARAMS.K=$K \
+    config.MODEL.WEIGHTS_INIT.PARAMS_FILE=/home/jan.olszewski/git/vissl/checkpoints/supervised/vitb.pth \
+    config.MODEL.WEIGHTS_INIT.STATE_DICT_KEY_NAME=model \
+    config.MODEL.TRUNK.VISION_TRANSFORMERS.COMPVITS.COMP.NAME="afterK" \
+    config.MODEL.TRUNK.VISION_TRANSFORMERS.COMPVITS.COMP.PARAMS.K=$K \
+    config.MODEL.TRUNK.VISION_TRANSFORMERS.COMPVITS.SPLIT.NAME="precomputed_masks" \
+    config.MODEL.TRUNK.VISION_TRANSFORMERS.COMPVITS.SPLIT.PARAMS.M=2 \
+    config.DISTRIBUTED.NUM_PROC_PER_NODE=1 \
+    
